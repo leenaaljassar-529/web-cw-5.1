ec77abb8061cf7604ddf4e3cb11db957d3b52bf5
leenaaljassar-529 <66418005+leenaaljassar-529@users.noreply.github.com>
2020-06-27 19:55:04 +0300

    .

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,31 @@
 // السؤال الأول
 // 1
-
+const name = "لينا";
 // 2
-
+let age = 16;
 // 3 and 4
-
+console.log("انا " + " " + name + " و عمري" + age);
 // 5 and 6
-
+console.log((age *= 5));
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobbies = ["crocheting", "drawing", "crafting"];
 // 2
-
+hobbies.push("16");
+console.log(hobbies);
 // 3
-
+hobbies.pop("");
+console.log(hobbies);
 // 4
-
+let course = ["برمجة المواقع", "CSS"];
 // 5
-
+course.push("رهف");
 // 6
-
+console.log(course[0]);
 // 7
-
+console.log(course[2]);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
